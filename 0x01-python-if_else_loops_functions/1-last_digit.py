28786f1243daab8594fbe85b7a2693c4fdcb8128
Salaudeen Ibraheem Abbey <88151328+2tech-2@users.noreply.github.com>
2022-06-02 14:10:39 +0100

    commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
+from tkinter import N
 number = random.randint(-10000, 10000)
 if number >= 0:
     last_digit = number % 10
@@ -9,8 +10,8 @@ else:
 message = "Last digit of %d is %d and is" % (number, last_digit)
 
 if last_digit > 5:
-    print(f"{message} greater than 5")
+    print(f"{message} greater than 5""\n")
 elif last_digit == 0:
-    print(f"{message} 0")
+    print(f"{message} 0""\n")
 elif last_digit < 6:
-    print(f"{message} less than 6 and not 0")
+    print(f"{message} less than 6 and not 0""\n")
