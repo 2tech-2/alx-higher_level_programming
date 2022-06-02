b2c7aaa5459f5c64db03e6b2ea2870bd514c7beb
Salaudeen Ibraheem Abbey <88151328+2tech-2@users.noreply.github.com>
2022-06-02 13:57:29 +0100

    commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,6 @@ message = "Last digit of %d is %d and is" % (number, last_digit)
 if last_digit > 5:
     print(f"{message} greater than 5")
 elif last_digit == 0:
-    print("{message} 0")
+    print(f"{message} 0")
 elif last_digit < 6:
-    print(f"{message} less than 6 and not 0\n")
+    print(f"{message} less than 6 and not 0")
